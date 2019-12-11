b0ca17d6b79187e0cc86f433297bc64a10168e12
Gustavo Santiago <egustavo4@gmail.com>
2019-12-10 23:35:12 -0300

    configure docker

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ use Mix.Config
 
 # Configure your database
 config :server, Server.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "server_dev",
-  hostname: "localhost",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_NAME"),
+  hostname: System.get_env("DB_HOST"),
+  port: System.get_env("DB_PORT"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
